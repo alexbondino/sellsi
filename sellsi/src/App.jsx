f312b672c435eb0203a15850ac6eb8feb25c75a3
Daniel <96345639+alexbondino@users.noreply.github.com>
2025-05-21 17:57:29 -0400

    added dependencies

--- sellsi/src/App.jsx
+++ sellsi/src/App.jsx
@@ -1,6 +1,7 @@
 import { Box } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from './styles/theme';
+import React, { useEffect, useState } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import TestSupabase from './services/test-supabase';
 import BottomBar from './components/BottomBar';
