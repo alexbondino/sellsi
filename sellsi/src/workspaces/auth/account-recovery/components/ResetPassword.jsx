500c8ebb3e06cb59d4040dc6fb9b07eb88b40493
Daniel <96345639+alexbondino@users.noreply.github.com>
2025-10-08 23:39:10 -0300

    reset password

--- sellsi/src/workspaces/auth/account-recovery/components/ResetPassword.jsx
+++ sellsi/src/workspaces/auth/account-recovery/components/ResetPassword.jsx
@@ -1,4 +1,4 @@
-// src/domains/auth/pages/ResetPassword.jsx
+// src/workspaces/auth/account-recovery/components/ResetPassword.jsx
 import React, { useState } from 'react';
 import {
   Box,
@@ -10,8 +10,8 @@ import {
 } from '@mui/material';
 import LoadingButton from '@mui/lab/LoadingButton';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { supabase } from '../../../services/supabase';
-import { PasswordRequirements } from '../../../shared/components';
+import { supabase } from '@/services/supabase';
+import { PasswordRequirements } from '@/shared/components';
 
 export default function ResetPassword() {
   const [password, setPassword] = useState('');
